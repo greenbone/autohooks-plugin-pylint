6c4baf0d3a8fbf82e334083d8f90bc6f01b3c833
Jaspar L <jstach@uos.de>
2020-11-10 09:09:55 +0100

    Update tests/test_pylint.py

--- tests/test_pylint.py
+++ tests/test_pylint.py
@@ -20,7 +20,7 @@ import sys
 
 from io import StringIO
 from pathlib import Pathf
-rom unittest import TestCase
+from unittest import TestCase
 from unittest.mock import patch, MagicMock, Mock
 
 from autohooks.config import load_config_from_pyproject_toml
